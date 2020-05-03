c2cb5af334007ec65158f7502de9e801ba14d76f
DBurby <burby@hartford.edu>
2020-05-03 16:54:08 -0400

    Add "gulp dev" and "gulp build" tasks
    browser-sync development for "gulp dev" and creation of the "prod" folder for the "gulp build" task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,28 @@
+const { watch, src, series, dest } = require(`gulp`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
 
+let copyCSS = () => {
+    return src(`css/*.css`)
+        .pipe(dest(`prod/css/`));
+};
+
+let copyJS = () => {
+    return src(`js/*.js`)
+        .pipe(dest(`prod/js/`));
+};
+
+let dev = () => {
+    browserSync({
+        server: {
+            baseDir: [
+                `html`,
+                `prod`
+            ]
+        }
+    });
+    watch(`html/**/*.html`).on(`change`, reload);
+};
+
+exports.build = series(copyCSS, copyJS);
+exports.dev = dev;
