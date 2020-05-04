00e7c557854f961d6839f7a6ab7d3fe6b49cd98d
DBurby <burby@hartford.edu>
2020-05-04 10:40:24 -0400

    create ratio
    There was a mistake in the commit of the input box. The JS code I selected here is for that, while the code I submit there was for the ratio.

--- js/app.js
+++ js/app.js
@@ -19,6 +19,27 @@ for (let i = 0; i < tab.length; i++) {
     tab[i].addEventListener(`click`, onTabClick, false);
 }
 
+function getInput(){
+    const inputVal = document.getElementById(`theInput`).value;
+
+    let whiteRiceAmou = document.getElementById(`whiteRiceAmount`).value;
+    document.getElementById(`whiteRiceAmount`).value = inputVal*whiteRiceAmou;
+    let whiteRiceWate = document.getElementById(`whiteRiceWater`).value;
+    document.getElementById(`whiteRiceWater`).value = inputVal*whiteRiceWate;
+    let caliRiceAmou = document.getElementById(`caliRiceAmount`).value;
+    document.getElementById(`caliRiceAmount`).value = inputVal*caliRiceAmou;
+    let caliRiceWate = document.getElementById(`caliRiceWater`).value;
+    document.getElementById(`caliRiceWater`).value = inputVal*caliRiceWate;
+    let caliSoftWate = document.getElementById(`caliSoftWater`).value;
+    document.getElementById(`caliSoftWater`).value = inputVal*caliSoftWate;
+
+    if (inputVal == 1){
+        document.getElementById(`whiteRiceAmount`).value = 8;
+        document.getElementById(`whiteRiceWater`).value = 16;
+        document.getElementById(`caliRiceAmount`).value = 10;
+        document.getElementById(`caliRiceWater`).value = 16;
+        document.getElementById(`caliSoftWater`).value = 4;
+    }
 
     document.getElementById(`whiteRatio1`).value = inputVal;
     document.getElementById(`whiteRatio2`).value = inputVal * 2;
